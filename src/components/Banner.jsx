934bcde01e6db5b72ee68fb22adcaab5149e0e77
Mohammad Aasif <kasifsaif784@gmail.com>
2023-12-15 14:33:30 +0530

    chnages

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,16 @@ const Banner = () => {
 
   return (
     <div className="carousel position-relative">
-      <img className="w-100" style={{ height: '440px' }} src={imageUrl} alt="banner" />
-      <div className="text-light position-absolute translate-middle">
+      <img
+        className="w-100"
+        style={{ height: "440px" }}
+        src={imageUrl}
+        alt="banner"
+      />
+      <div
+        className="text-light position-absolute translate-middle bottom"
+        style={{ left: "48%", width: "80%", bottom: "-6%" }}
+      >
         <h1>Computer Engineering</h1>
         <p>142,765 Computer Engineers follow this</p>
       </div>
